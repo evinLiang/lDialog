8094ff41f04ed76c695dc8ced50bc4c11badd10c
evinLiang <evin@evinweb.com>
2018-07-10 13:30:34 +0800

    增加了加载toast

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # lDialog
 lDialog-会话框，依赖jquery
 
+> 主题色：theme:["default","primary","link","success","info","warning","danger"]
+
 ### toast使用
 ```
-//主题色：theme:["default","primary","link","success","info","warning","danger"]
-
 $.lDialog.toast({
-	content:'我是一个toast',		//toast内容
-	duration: 2000,		//显示时间，默认为2500秒
-	theme:'default'		//主题色，默认为default
+  content:'我是一个toast',	//toast内容,默认"我是一个toast"
+  duration: 2000,		//显示时间，默认为2500秒
+  theme:'default'		//主题色，默认为default
 });
+```
+
+### iconToast使用，icon类型有：["success"、"error"]
+```
+$.lDialog.iconToast({
+  title: '已完成',	//toast标题,默认"我是一个iconToast"
+  icon: 'success',	//toast-icon类型，默认为"success"
+  theme:'default',	//主题色，默认为default	
+  duration: 2000	//显示时间，默认为2500秒
+})
+```
+
+### loadingToast使用
+```
+//显示加载框
+$.lDialog.sLoadingToast({
+  title: '加载中',	//toast标题，默认"加载中"
+  theme:'default',	//主题色，默认为default	
+})
+//关闭加载框
+$.lDialog.hLoadingToast()
 ```
--- lDialog.js
+++ lDialog.js
@@ -1,5 +1,6 @@
 ;(function ( $, window, document, undefined ){
 	$.lDialog = {
+
 		//弹出提示框
         toast: function (req) {
 
@@ -17,6 +18,44 @@
             setTimeout(function(){
             	$(".lDialog-toast").remove();
             },duration)
+        },
+
+        //弹出icon提示框
+        iconToast: function (req) {
+
+            //判断toast是否已经显示,防止二次点击再次生成
+            if($(".lDialog-iconToast").is(":visible")){
+                return
+            }
+            var title = req.title || "我是一个iconToast",   //toast标题
+            duration = req.duration || 2500,           //toast显示时间,默认2秒
+            icon = req.icon || "success";              //icon类型，默认为"success"，一共有2种,icon:["success","error"]
+            theme = req.theme || "default";            //主题颜色,一共有7种,theme:["default","primary","link","success","info","warning","danger"]
+            $("body").append("<div class='lDialog lDialog-iconToast "+theme+"'><div class='iconToast-icon "+icon+"'><img src='"+icon+".png' alt=''></div><p class='iconToast-text'>"+title+"</p></div>"); //显示toast
+            $(".lDialog-iconToast").fadeIn(500);
+
+            //显示后关闭toast
+            setTimeout(function(){
+                $(".lDialog-iconToast").remove();
+            },duration)
+        },
+
+        //弹出loading提示框
+        sLoadingToast: function(req){
+
+           //判断toast是否已经显示,防止二次点击再次生成
+            if($(".lDialog-LoadingToast").is(":visible")){
+                return
+            } 
+            var title = req.title || "加载中",   //toast标题，默认"加载中"
+            theme = req.theme || "default";            //主题颜色,一共有7种,theme:["default","primary","link","success","info","warning","danger"]
+            $("body").append("<div class='lDialog lDialog-LoadingToast "+theme+"'><div class='loading'><img src='loading.png' alt=''></div><p class='iconToast-text'>"+title+"</p></div>"); //显示toast
+            $(".lDialog-LoadingToast").fadeIn(500);
+        },
+
+        //关闭loading提示框
+        hLoadingToast: function(){
+            $(".lDialog-LoadingToast").remove();
         }
 	}
 })(jQuery, window,document);
