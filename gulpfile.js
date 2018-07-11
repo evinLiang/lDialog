6caba49ec13b3e423cc27fef727a14f80fd0e9a9
evinLiang <evin@evinweb.com>
2018-07-11 22:45:56 +0800

    gulp-webserver

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var cssmin = require('gulp-cssmin');	//压缩css
 var uglify = require('gulp-uglify');	//js压缩
 var connect = require("gulp-connect");  //server
 var autoprefixer = require('gulp-autoprefixer'); //兼容css浏览器前缀
-var ext_replace = require('gulp-ext-replace');	//
+var ext_replace = require('gulp-ext-replace');	//文件命名
+var webserver = require('gulp-webserver'); // 本地服务器
 
 //lDialog.less文件处理后保存到，./dist/css 文件夹下
 gulp.task('less', function () {
@@ -38,6 +39,12 @@ gulp.task('jsmin', function () {
         .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('default', function () {
-  console.log(1);
+//server服务器
+gulp.task('webserver', function() {
+  gulp.src( './' ) // 服务器目录（代表根目录）
+  .pipe(webserver({
+    livereload: true,
+  }));
 });
+
+gulp.task('default',['webserver','watch']);
